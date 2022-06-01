c220fb290bde3501e97ad78ad026e713e0076a0a
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-06-01 15:33:12 +0300

    add local env & connect firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
