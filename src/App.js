6e5cbd23fe9f3de7a26dc18f00cf4566cbb0497d
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-06-02 19:43:54 +0300

    connect firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home.jsx';
 import { NotFound } from './pages/NotFound.jsx';
 
+import { getDatabase, ref, child, get } from 'firebase/database';
+import './firebase.config';
+
 import './App.scss';
 
 function App() {
+  useEffect(() => {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+        } else {
+          console.log('No data available');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
