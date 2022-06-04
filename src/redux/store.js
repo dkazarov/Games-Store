2fb1c9b1a423f500694cd1d10ba0a4b3460b3949
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-06-04 17:29:41 +0300

    add cart slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dataReducer from './slices/dataSlice';
+import cartReducer from './slices/cartSlice';
 
 export default configureStore({
   reducer: {
     data: dataReducer,
+    cart: cartReducer,
   },
 });
