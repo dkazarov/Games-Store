11d02c0c5a9579e375660a0fff815aeef270b852
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-06-08 16:02:30 +0300

    add delete product from preview cart

--- src/components/CartPreview/CartPreview.jsx
+++ src/components/CartPreview/CartPreview.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteFromCart } from '../../redux/slices/cartSlice';
 
 import style from './CartPreview.module.scss';
 import Box from '@mui/material/Box';
@@ -11,8 +12,13 @@ import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { Link } from 'react-router-dom';
 
 export const CartPreview = () => {
+  const dispatch = useDispatch();
   const productsInCart = useSelector((state) => state.cart.cart);
   const previewCartWindow = useSelector((state) => state.cart.previewCart);
+
+  const deleteProductFromCart = (id) => {
+    dispatch(deleteFromCart(id));
+  };
 
   return (
     <>
@@ -21,12 +27,15 @@ export const CartPreview = () => {
           <ul className={style.ul}>
             {productsInCart.map((products) => (
               <li key={nanoid()} className={style.list}>
-                <HighlightOffIcon sx={{ cursor: 'pointer', mr: 2 }} />
+                <HighlightOffIcon
+                  onClick={() => deleteProductFromCart(products.id)}
+                  sx={{ cursor: 'pointer', mr: 2 }}
+                />
                 <img src={products.image} alt='image' className={style.image} />
                 <Box className={style.info}>
                   <Box className={style.title}>{products.title}</Box>
                   <Box className={style.price}>{products.price} грн</Box>
-                  <Link to='/cart' className={style.link} >
+                  <Link to='/cart' className={style.link}>
                     <Button variant='contained' size='small' sx={{ display: 'block', m: '0 auto' }}>
                       Купити
                     </Button>
