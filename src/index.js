558f9480618cab91dfe632c0b3d311f8cb41f110
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-06-22 15:29:39 +0300

    predeploy

--- src/index.js
+++ src/index.js
@@ -13,12 +13,11 @@ import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
+    <HashRouter>
+      <Provider store={store}>
         <App />
-      </HashRouter>
-      ,
-    </Provider>
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
 );
 
